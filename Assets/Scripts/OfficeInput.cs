3b0d9b6006fb97b0200d1a8e4778e1f874a3d1b3
Spanish Freddy <freddytutoriales2000@gmail.com>
2025-06-02 21:48:53 +0200

    Working on Office

---
+++ Assets/Scripts/OfficeInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class OfficeInput : MonoBehaviour
+{
+    public float edgeThreshold = 100f;
+    public float rotationSpeed = 50f;
+    public float minYaw = -60f;
+    public float maxYaw = 60f;
+
+    private float currentYaw;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        currentYaw = transform.localEulerAngles.y;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        float screenWidth = Screen.width;
+
+        if (mousePos.x <= edgeThreshold)
+        {
+            currentYaw -= rotationSpeed * Time.deltaTime;
+        }
+        else if (mousePos.x >= screenWidth - edgeThreshold)
+        {
+            currentYaw += rotationSpeed * Time.deltaTime;
+        }
+
+        currentYaw = Mathf.Clamp(currentYaw, minYaw, maxYaw);
+        transform.localEulerAngles = new Vector3(0f, currentYaw, 0f);
+    }
+}
